97f56886e35c48424f94dfc26e908d7200f23b00
siphr <work@0blink.com>
2013-11-21 14:33:46 +0000

    > Added documentation block for isPalindrome method. > Added extra case for empty strings. >> Assumed to be palindromes. > Added extra case for 1 character strings. >> Not needed as such but facilitates an early exit. >> Assumed to be palindromes.

--- palindrome.cpp
+++ palindrome.cpp
@@ -9,26 +9,45 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/**
+ * \brief
+ * Checks to see if the given string is a palindrome.
+ *
+ * \details
+ * Iterates  by i over the string of size s to match character at i against character at s - i.
+ * As I do 2 checks per loop I only need to iterate upto half the string length.
+ * Odd lengths do not matter hence used integer division.
+ * An empty string or a string of single character in this method is assumed to be a palindrome. This could
+ * be turned around to return false or even throw an exception.
+ *
+ * \param[in] string
+ * A pointer to null terminated const string.
+ *
+ * \return
+ * True if the word is a palindrome, False otherwise.
+ */
 bool isPalindrome(const char *string)
 {
 	// assuming string is null terminated
 	size_t count = strlen(string);
 
+    // check if string is empty or of single character
+    if (count == 0 || count == 1)
+    {
+        return true;
+    }
+    
 	// I only need to iterate through half of the string.
 	// using integer division as I do not care about the middle character in case of odd length strings
 	const size_t half_count = count/2;	
 
-	//printf("%lu\n",count);
-
-	// starting for the last element
+	// Initialising for the last element
 	count--;
 
 	// for each character upto half string length...
 	for (size_t i=0; i < half_count; count--, i++)
 	{
-		//printf("%c\n", *(string+i));
-		//printf("%c\n", *(string+count));
-
 		// ... check if it matches the character at the mirrored position at the end
 		if (*(string+i) != *(string + count))
 		{
@@ -42,6 +61,8 @@ bool isPalindrome(const char *string)
 int main(int argc, char **argv)
 {
 	bool pass = 
+		isPalindrome("") &&
+		isPalindrome("a") &&
 		isPalindrome("racecar") &&
 		!isPalindrome("notapalindrome") &&
 		isPalindrome("aaaaAAAAaaaa") &&
