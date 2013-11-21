2c61d55697862ba9925cc2633720bc1039656f0a
siphr <work@0blink.com>
2013-11-21 12:34:05 +0000

    > Palindrome first pass. > Pointers based implementation as the code seemed to suggest C. >> Pretty standard implementation. >> TODO: Revisit documentation and perhaps cleanup.

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,11 +7,36 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 bool isPalindrome(const char *string)
 {
-	// Implement me!
-	return false;
+	// assuming string is null terminated
+	size_t count = strlen(string);
+
+	// I only need to iterate through half of the string.
+	// using integer division as I do not care about the middle character in case of odd length strings
+	const size_t half_count = count/2;	
+
+	//printf("%lu\n",count);
+
+	// starting for the last element
+	count--;
+
+	// for each character upto half string length...
+	for (size_t i=0; i < half_count; count--, i++)
+	{
+		//printf("%c\n", *(string+i));
+		//printf("%c\n", *(string+count));
+
+		// ... check if it matches the character at the mirrored position at the end
+		if (*(string+i) != *(string + count))
+		{
+			return false;
+		}
+	}
+	
+	return true;
 }
 
 int main(int argc, char **argv)
